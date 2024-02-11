27c70d15520a45d73a9833f9ba38aeb3d60e652b
gasparschott <mshroud@protonmail.com>
2024-02-11 13:22:01 -0800

    Version 1.0.1
    Disabled scroll into view when using arrows to navigate between notes.
    Fixed arrow navigation between non-markdown notes (e.g., images, pdf, media files, etc.)
    Added Continuous mode menu item to the tab-group menu (thanks to Gorkycreator for the suggestion)

--- main.js
+++ main.js
@@ -2,14 +2,14 @@
 
 let obsidian = require('obsidian');
 
-const DEFAULT_SETTINGS = {
+let DEFAULT_SETTINGS = {
 	'tabGroupIds': []
 };
 
 class ContinuousModePlugin extends obsidian.Plugin {
     async onload() {
 		console.log('Loading the Continuous Mode plugin.');
-		await this.loadSettings();
+		await this.loadSettings();		
 		/* ----------------------- */
 		// HELPERS
 		const getAllTabGroups = (begin_node,all_tabs) => {
@@ -55,14 +55,16 @@ class ContinuousModePlugin extends obsidian.Plugin {
 		// TOGGLE CONTINUOUS MODE
 		const toggleContinuousMode = (tab_group_id) => {
 			tab_group_id = tab_group_id ?? getActiveTabGroup().containerEl.dataset.tab_group_id;		// use provided tabGroupId from stored settings or use activeTabGroupId from toggle command
-			switch(true) {
-				case getTabGroupById(tab_group_id)?.containerEl?.classList.contains('is_continuous_mode'):						// if tab group is in continuous mode
-					getTabGroupById(tab_group_id)?.containerEl?.classList.remove('is_continuous_mode');							// remove style
-					this.settings.tabGroupIds.splice(this.settings.tabGroupIds.indexOf(tab_group_id),1);						// remove tabGroupdId from data.json
-					break;
-				default:																										// if tab group is not in continuous mode (e.g., on app launch)
-					getTabGroupById(tab_group_id)?.containerEl?.classList.add('is_continuous_mode');							// add style
-					if ( !this.settings.tabGroupIds.includes(tab_group_id) ) { this.settings.tabGroupIds.push(tab_group_id); }	// add tabGroupdId to data.json if it is not already there
+			if ( this.app.appId === tab_group_id.split('_')[0] ) {
+				switch(true) {
+					case getTabGroupById(tab_group_id)?.containerEl?.classList.contains('is_continuous_mode'):						// if tab group is in continuous mode
+						getTabGroupById(tab_group_id)?.containerEl?.classList.remove('is_continuous_mode');							// remove style
+						this.settings.tabGroupIds.splice(this.settings.tabGroupIds.indexOf(tab_group_id),1);						// remove tabGroupdId from data.json
+						break;
+					default:																										// if tab group is not in continuous mode (e.g., on app launch)
+						getTabGroupById(tab_group_id)?.containerEl?.classList.add('is_continuous_mode');							// add style
+						if ( !this.settings.tabGroupIds.includes(tab_group_id) ) { this.settings.tabGroupIds.push(tab_group_id); }	// add tabGroupdId to data.json if it is not already there
+				}
 			}
 			this.settings.tabGroupIds = [...new Set(this.settings.tabGroupIds)];					// remove dupe IDs if necessary
 			this.settings.tabGroupIds.sort();																					// sort the tabGroupIds
@@ -86,57 +88,51 @@ class ContinuousModePlugin extends obsidian.Plugin {
 				if ( getTabHeaderIndex(f) !== initialTabHeaderIndex ) { rearrangeLeaves(f,initialTabHeaderIndex); }		// only rearrange leaves if tab header is actually moved to a new position
 			}
 		}
-		const scrollActiveLeafIntoView = () => {
-			let el = getActiveLeaf()?.containerEl;
+		const scrollActiveLeafIntoView = (e) => {
+			let el = getActiveLeaf()?.containerEl, scroll_block = ( e?.key && /down|right/.test(e.key) ? 'top' : e?.key && /up|left/.test(e.key) ? 'end' : 'top' );
 			el = ( getActiveTabGroup().containerEl.classList.contains('hide_note_titles') ? el?.querySelector('.cm-editor') : el.querySelector('.view-header') );
-			el.scrollIntoView();
+			el.scrollIntoView({behavior:'smooth' });
 		}
 		// REARRANGE LEAVES on dragend
 		const rearrangeLeaves = (e,initialTabHeaderIndex) => {
 			let this_tab_container = e.target.closest('.workspace-tabs').querySelector('.workspace-tab-container');		// get current tab container
-			let leaves = Array.from(this_tab_container.querySelectorAll('.workspace-leaf'));							// get current tab container leaves
+			let leaves = Array.from(this_tab_container.children);														// get current tab container leaves
 			let finalTabHeaderIndex = getTabHeaderIndex(e);																// get final dropped tab header index
-			let rearranged = '';																						// define rearranged leaves variable
 			let moved = leaves.splice(initialTabHeaderIndex,1);															// get the moved leave
-			leaves.toSpliced(finalTabHeaderIndex,0,moved[0]);															// move the moved leaf into position
-			leaves.forEach( leaf => rearranged += leaf.outerHTML );														// compose rearranged HTML
-			this_tab_container.innerHTML = rearranged;																	// replace tab container content with rearranged leaves
+			let rearranged = leaves.toSpliced(finalTabHeaderIndex,0,moved[0]);											// move the moved leaf into position
+			this_tab_container.setChildrenInPlace(rearranged);															// replace tab container content with rearranged leaves
 			getTabGroupHeaders()[finalTabHeaderIndex].click();															// confirm drag and focus leaf by clicking tab
 		}
 		// ARROW NAVIGATION between open leaves
 		const leafArrowNavigation = (e) => {
-			if ( getActiveLeaf().containerEl.closest('.workspace-split.mod-root') === null ) { return; }								// return if not in leaf editor
+console.log(this.app.workspace);
+			if ( getActiveLeaf()?.containerEl?.closest('.workspace-split.mod-root') === null && !getActiveEditor()?.hasFocus() ) { return; }	// return if not in leaf editor or editor not focussed
 			let cursorAnchor = getActiveEditor()?.getCursor('anchor');
 			let activeTabGroupChildren = getActiveLeaf().workspace.activeTabGroup.children;
-			switch(true) {
-				case ( /Arrow/.test(e.key) && !e.altKey && !e.ctrlKey && !e.metaKey && !e.shiftKey ):					// Arrow navigation between leaves
-					switch(e.key) {
-						case 'ArrowUp': case 'ArrowLeft':
-							switch(true) {
-								case e.target.classList.contains('inline-title') && window.getSelection().anchorOffset === 0:									// cursor in inline-title
-								case e.target.classList.contains('metadata-properties-heading'):																// cursor in properties header
-								case cursorAnchor?.line === 0 && cursorAnchor?.ch === 0:																		// cursor at first line, first char
-								case getActiveLeaf().getViewState().state.mode === 'preview':																	// leaf is in preview mode
-								case (!/markdown/.test(getActiveLeaf().getViewState().type)):																	// leaf is empty (new tab)
-									if ( getActiveLeaf().containerEl.previousSibling !== null ) {																// ignore if first leaf
-										this_workspace.setActiveLeaf(activeTabGroupChildren[activeTabGroupChildren.indexOf(getActiveLeaf()) - 1],{focus:true});	// make previous leaf active 
-										getActiveEditor()?.setCursor({line:getActiveEditor().lastLine(),ch:getActiveEditor().lastLine().length - 1});			// select last char
-									}
-									break;
-							}
-							break;
-						case 'ArrowDown':	case 'ArrowRight': 
-							switch(true) {
-								case ( cursorAnchor?.ch === getActiveEditor()?.getLine(getActiveEditor().lastLine()).length && cursorAnchor?.line === getActiveEditor()?.lineCount() - 1 ):
-								case getActiveLeaf().getViewState().state.mode === 'preview':																	// leaf is in preview mode
-								case (!/markdown/.test(getActiveLeaf().getViewState().type)):																	// make next leaf active 
-									this_workspace.setActiveLeaf((activeTabGroupChildren[activeTabGroupChildren.indexOf(getActiveLeaf()) + 1] || getActiveLeaf()),{focus:true}); 
-									break;
+			switch(e.key) {
+				case 'ArrowUp': case 'ArrowLeft':
+					switch(true) {
+						case e.target.classList.contains('inline-title') && window.getSelection().anchorOffset === 0:									// cursor in inline-title
+						case e.target.classList.contains('metadata-properties-heading'):																// cursor in properties header
+						case cursorAnchor?.line === 0 && cursorAnchor?.ch === 0:																		// cursor at first line, first char
+						case getActiveLeaf().getViewState().state.mode === 'preview':																	// leaf is in preview mode
+						case (!/markdown/.test(getActiveLeaf().getViewState().type)):																	// leaf is empty (new tab)
+							if ( getActiveLeaf().containerEl.previousSibling !== null ) {																// ignore if first leaf
+								this_workspace.setActiveLeaf(activeTabGroupChildren[activeTabGroupChildren.indexOf(getActiveLeaf()) - 1],{focus:true});	// make previous leaf active 
+								getActiveEditor()?.setCursor({line:getActiveEditor().lastLine(),ch:getActiveEditor().lastLine().length - 1});			// select last char
 							}
 							break;
 					}
 					break;
-				// Start another keydown case here
+				case 'ArrowDown':	case 'ArrowRight': 
+					switch(true) {
+						case ( cursorAnchor?.ch === getActiveEditor()?.getLine(getActiveEditor().lastLine()).length && cursorAnchor?.line === getActiveEditor()?.lineCount() - 1 ):
+						case getActiveLeaf().getViewState().state.mode === 'preview':																	// leaf is in preview mode
+						case (!/markdown/.test(getActiveLeaf().getViewState().type)):																	// make next leaf active 
+							this_workspace.setActiveLeaf((activeTabGroupChildren[activeTabGroupChildren.indexOf(getActiveLeaf()) + 1] || getActiveLeaf()),{focus:true}); 
+							break;
+					}
+					break;
 			}
 		}
 		// REGISTER EVENTS
@@ -150,9 +146,9 @@ class ContinuousModePlugin extends obsidian.Plugin {
 			}
 		});
 		this.registerDomEvent(document,'keydown', function (e) {
-			if ( !e.target.closest('.workspace-tabs')?.classList.contains('is_continuous_mode')) { return; }
-			if ( !getActiveLeaf().containerEl.closest('.workspace-tabs')?.classList.contains('is_continuous_mode') ) { return; }
-			leafArrowNavigation(e); 
+			if ( e.target.tagName === 'body' )													 							{ return; }	// do nothing if tab group is not active
+			if ( !getActiveLeaf().containerEl.closest('.workspace-tabs')?.classList.contains('is_continuous_mode') )		{ return; }	// do nothing if continuous mode is not active in tab group
+			if ( /Arrow/.test(e.key) && !e.altKey && !e.ctrlKey && !e.metaKey && !e.shiftKey ) { leafArrowNavigation(e); }				// else arrow navigation
 		});	
 		this.registerDomEvent(document,'dragstart',function(e) { 
 			if ( !e.target.closest('.workspace-tabs')?.classList.contains('is_continuous_mode')) { return; }
@@ -164,7 +160,7 @@ class ContinuousModePlugin extends obsidian.Plugin {
 				.setSection('pane')
 				.setChecked( getActiveTabGroup().containerEl.classList.contains('is_continuous_mode') ? true : false )
 				.onClick(async () => { 
-					toggleContinuousMode(getActiveTabGroup().containerEl.dataset.tab_group_id || '') }
+					toggleContinuousMode(getActiveTabGroup().containerEl.dataset.tab_group_id) }
 			);
 		}
 		this.registerEvent(
@@ -179,20 +175,22 @@ class ContinuousModePlugin extends obsidian.Plugin {
 				menu.addItem((item) => { addContinuousModeMenuItem(item,'editor-menu') });
 			})
 		);
+		this.registerEvent(
+			this.app.workspace.on('tab-group-menu', (menu) => {
+				menu.addItem((item) => { addContinuousModeMenuItem(item,'editor-menu') });
+			})
+		);
 		this.registerEvent(													// initContinuousMode on layout change
 			this.app.workspace.on('layout-change', async () => { 
 				updateTabGroupIds(); 
 				updateDataTabGroupIds(); 
 			})
 		);
-		this.registerEvent(													// initContinuousMode on layout ready
-			this.app.workspace.on('layout-ready', async () => { 
-//			this.app.workspace.onLayoutReady(() => {
-				updateTabGroupIds(); 
-				updateDataTabGroupIds();
-				initContinuousMode();
-			})
-		);
+		this.app.workspace.onLayoutReady( async () => {						// initContinuousMode on layout ready
+			updateTabGroupIds(); 
+			updateDataTabGroupIds();
+			initContinuousMode();
+		})
 		// ADD COMMAND PALETTE ITEMS
 		this.addCommand({																				// add command: toggle continuous mode in active tab group
 			id: 'toggle-continuous-mode-active',
